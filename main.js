128e0b2bb3e9ea635efed795386a00ce08563f91
mikensothomas <thomasmikenson40@gmail.com>
2022-12-11 22:51:21 -0300

    Curso de JavaScript

---
+++ main.js
@@ -0,0 +1,8 @@
+//var message = 'Hello world'
+//alert('Hello wold')
+//alert(message)
+
+const ferstName = 'Mikenson'
+const lastName = 'Thomas'
+console.log('meu nome é: ' + ferstName + ' ' + lastName)
+console.log(`Meu nome é: ${ferstName.toUpperCase()} ${lastName.toLowerCase()}`)
