71c23ff6a0fb9aa374fe9de0d9b67ad6c14cda6b
mikensothomas <thomasmikenson40@gmail.com>
2022-12-11 23:32:45 -0300

    segundoCommit

--- main.js
+++ main.js
@@ -2,7 +2,26 @@
 //alert('Hello wold')
 //alert(message)
 
-const ferstName = 'Mikenson'
-const lastName = 'Thomas'
-console.log('meu nome é: ' + ferstName + ' ' + lastName)
-console.log(`Meu nome é: ${ferstName.toUpperCase()} ${lastName.toLowerCase()}`)
+//const ferstName = 'Mikenson'
+//const lastName = 'Thomas'
+//console.log('meu nome é: ' + ferstName + ' ' + lastName)
+//console.log(`Meu nome é: ${ferstName.toUpperCase()} ${lastName.toLowerCase()}`)
+
+//const names = "Mikenson,shella,Guerlande,Elynda, Loulouse,Diomelie,Luc,Shelove";
+//console.log(names.split(","));
+
+//NUMBER
+//const number = 5;
+//console.log(number.toString());
+//console.log(typeof number.toString());
+
+//BOOLEAN
+//false
+//true
+//console.log(2 == 1);
+//console.log(2 == 2);
+
+//NULL & UNDEFINED
+
+//const x = null;
+//const y = undefined;
